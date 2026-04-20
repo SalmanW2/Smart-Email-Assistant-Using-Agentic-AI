b6e08b1cabb7a439de34b6d3898a44c9b187d8c8
MUHAMMAD SALMAN WATTOO <160517373+SalmanW2@users.noreply.github.com>
2026-04-20 16:37:22 +0500

    Refactor Telegram and Gemini config variables

--- config_env.py
+++ config_env.py
@@ -3,15 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Telegram Config
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_TELEGRAM_ID = int(os.getenv("OWNER_TELEGRAM_ID", "0"))
-WEBHOOK_URL = os.getenv("RENDER_WEB_SERVICE_URL") # For fixing conflict error
-
-# Gemini Config
+WEBHOOK_URL = os.getenv("RENDER_WEB_SERVICE_URL") 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-# Auth Config
 CREDENTIALS_FILE = "credentials.json"
 TOKEN_FILE = "token.json"
 SCOPES = ['https://mail.google.com/']
